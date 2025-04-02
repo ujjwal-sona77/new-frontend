ab63fb83bd3a8baec260079f0537dec5a50863b8
Ujjwal Sonawat <146330690+ujjwal-sona77@users.noreply.github.com>
2025-04-02 20:50:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from "./components/Login.jsx";
 import Shop from "./components/Shop.jsx";
 import Checkout from "./components/Checkout.jsx";
 import CreateProduct from "./components/CreateProduct.jsx";
-import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
 import Cart from "./components/Cart.jsx";
 import Profile from "./components/Profile.jsx";
 import EditProfile from "./components/EditProfile.jsx";
@@ -15,13 +14,12 @@ const App = () => {
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route element={<ProtectedRoutes />}>
+
         <Route path="/profile" element={<Profile />} />
         <Route path="/users/edit-profile" element={<EditProfile />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/checkout" element={<Checkout />} />
-      </Route>
 
       <Route element={<AdminProtectedRoute />}>
         <Route path="/owner/createproduct" element={<CreateProduct />} />
